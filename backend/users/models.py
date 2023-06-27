d468431c92baea17c7d421e3fac16b2dc9da7aa5
Alena Chelnokova <achelnokova-00@yandex.ru>
2023-06-27 17:23:01 +0300

    27

--- backend/users/models.py
+++ backend/users/models.py
@@ -31,6 +31,9 @@ class CustomUser(AbstractUser):
         verbose_name='Пароль'
     )
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
     class Meta:
         verbose_name = 'Пользователь',
         verbose_name_plural = 'Пользователи',
