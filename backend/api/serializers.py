d6b553268abe28daf67046faa3e2a2563c6693e1
Alena Chelnokova <achelnokova-00@yandex.ru>
2023-06-30 01:04:29 +0300

    30

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -139,7 +139,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         ingredients = validated_data.pop('ingredients')
         tags = validated_data.pop('tags')
-        author = self.context['request'].user
+        author = self.request.user
 
         recipe = Recipe.objects.create(**validated_data,
                                        author=author,
