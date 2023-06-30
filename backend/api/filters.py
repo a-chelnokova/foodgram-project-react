7e9a55e3b4db20bd942cd1dfd681d3d32f449b33
Alena Chelnokova <achelnokova-00@yandex.ru>
2023-06-30 22:18:53 +0300

    30

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -18,18 +18,18 @@ class RecipeFilter(filter.FilterSet):
     )
     is_favorited = filter.NumberFilter(method='get_favorite')
     is_in_shopping_cart = filter.NumberFilter(
-        method='get_is_in_shopping_cart')
+        method='get_in_shopping_cart')
 
     class Meta:
         model = Recipe
-        fields = ['tags', 'author', 'is_favorited', 'is_in_shopping_cart']
+        fields = ['tags', 'author', 'is_favorited', 'get_in_shopping_cart']
 
     def get_favorite(self, queryset, name, value):
         if value:
             return queryset.filter(favorites__user=self.request.user)
         return queryset
 
-    def get_is_in_shopping_cart(self, queryset, name, value):
+    def get_in_shopping_cart(self, queryset, name, value):
         if value:
             return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
