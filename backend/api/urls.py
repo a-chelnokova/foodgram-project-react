fcc32dfee994ee8c36b23dbd9485a93ff2a308df
Alena Chelnokova <achelnokova-00@yandex.ru>
2023-07-01 18:38:53 +0300

    AddAndDeleteSubscribe

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
-from users.views import SubscriptionListViewSet, SubscriptionViewSet
+from users.views import AddAndDeleteSubscribe
 
 app_name = 'api'
 
@@ -17,8 +17,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    path('users/subscriptions', SubscriptionListViewSet.as_view(),
-         name='users/subscriptions'),
-    path('users/<int:id>/subscribe/', SubscriptionViewSet.as_view(),
+    path('users/<int:user_id>/subscribe/',
+         AddAndDeleteSubscribe.as_view(),
          name='subscribe'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,54 +1,51 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import generics, status, views
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics, status
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
+from django.db.models.aggregates import Count
+from django.db.models.expressions import Value
 
-from api.serializers import ShowSubscriptionSerializer, SubscriptionSerializer
-from users.models import CustomUser, Subscription
-from api.pagination import CustomPagination
-
-
-class SubscriptionViewSet(views.APIView):
-    """Вьюсет для модели Subscription."""
-
-    permission_classes = [IsAuthenticated, ]
-    pagination_class = CustomPagination
-
-    def post(self, request, pk):
-        author = get_object_or_404(CustomUser, pk=pk)
-        user = self.request.user
-        data = {
-            'author': author.id,
-            'user': user.id,
-        }
-        serializer = SubscriptionSerializer(
-            data=data,
-            context={'request': request},
-        )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def delete(self, request, pk):
-        author = get_object_or_404(CustomUser, pk=pk)
-        subscription = get_object_or_404(
-            Subscription,
-            user=self.request.user,
-            author=author
-        )
-        subscription.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+from api.serializers import SubscribeSerializer
 
 
-class SubscriptionListViewSet(generics.ListAPIView):
-    """Вьюсет для отображения подписок пользователя."""
+User = get_user_model()
 
-    permission_classes = [IsAuthenticated, ]
-    pagination_class = CustomPagination
-    serializer_class = ShowSubscriptionSerializer
+
+class AddAndDeleteSubscribe(
+        generics.RetrieveDestroyAPIView,
+        generics.ListCreateAPIView):
+    """Подписка и отписка от пользователя."""
+
+    serializer_class = SubscribeSerializer
 
     def get_queryset(self):
-        user = self.request.user
-        return user.subscriber.all()
+        return self.request.user.follower.select_related(
+            'following'
+        ).prefetch_related(
+            'following__recipe'
+        ).annotate(
+            recipes_count=Count('following__recipe'),
+            is_subscribed=Value(True), )
+
+    def get_object(self):
+        user_id = self.kwargs['user_id']
+        user = get_object_or_404(User, id=user_id)
+        self.check_object_permissions(self.request, user)
+        return user
+
+    def create(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if request.user.id == instance.id:
+            return Response(
+                {'errors': 'На самого себя не подписаться!'},
+                status=status.HTTP_400_BAD_REQUEST)
+        if request.user.follower.filter(author=instance).exists():
+            return Response(
+                {'errors': 'Уже подписан!'},
+                status=status.HTTP_400_BAD_REQUEST)
+        subs = request.user.follower.create(author=instance)
+        serializer = self.get_serializer(subs)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def perform_destroy(self, instance):
+        self.request.user.follower.filter(author=instance).delete()
