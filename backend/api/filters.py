84fad58225f44ff55c0940b79ad8de4eaafe85db
Alena Chelnokova <achelnokova-00@yandex.ru>
2023-07-02 20:53:57 +0300

    0

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import FilterSet, filters
-from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Ingredient, Recipe
 from users.models import CustomUser
 
 
