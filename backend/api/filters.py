96ca3c6a4ffef8dafc70c8d30543982ec81decf8
Alena Chelnokova <achelnokova-00@yandex.ru>
2023-07-02 23:35:34 +0300

    0

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -13,11 +13,7 @@ class IngredientFilter(FilterSet):
 
 class RecipeFilter(FilterSet):
     author = filters.ModelChoiceFilter(queryset=CustomUser.objects.all())
-    tags = filters.ModelMultipleChoiceFilter(
-        field_name='tags__id',
-        queryset=Tag.objects.all(),
-        to_field_name='id',
-    )
+    tags = filters.CharFilter(field_name='tags__slug', method='filter_tags')
 
     is_favorited = filters.NumberFilter(
         method='filter_is_favorited'
@@ -26,6 +22,10 @@ class RecipeFilter(FilterSet):
         method='filter_is_in_shopping_cart'
     )
 
+    def filter_tags(self, queryset, slug, tags):
+        tags = self.request.query_params.getlist('tags')
+        return queryset.filter(tags__slug__in=tags).distinct()
+
     def filter_is_favorited(self, queryset, name, value):
         if value and not self.request.user.is_anonymous:
             return queryset.filter(favorite_user=self.request.user)
@@ -33,7 +33,7 @@ class RecipeFilter(FilterSet):
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
         if value and not self.request.user.is_anonymous:
-            return queryset.filter(shopping_cart_user=self.request.user)
+            return queryset.filter(shopping_cart__user=self.request.user)
         return queryset
 
     class Meta:
