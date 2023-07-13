c313f503219742b575c24472cc3447fa1f0a4795
Alena Chelnokova <achelnokova-00@yandex.ru>
2023-07-13 20:35:41 +0300

    -

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -31,7 +31,7 @@ class RecipeFilter(filter.FilterSet):
 
     def filter_is_favorited(self, queryset, name, value):
         if value and not self.request.user.is_anonymous:
-            return queryset.filter(favorites__user=self.request.user)
+            return queryset.filter(is_favorited__user=self.request.user)
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
